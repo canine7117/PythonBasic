af339a9de15fca2ad6b4222225dc22ee06f50058
canine7117 <95954842+canine7117@users.noreply.github.com>
2021-12-11 16:53:37 +0900

    Create main1.py

---
+++ 1강/main1.py
@@ -0,0 +1,138 @@
+print('내 \'첫 코딩\' 입니다.')
+print("내 '첫 코딩' 입니다.")
+
+print("오늘은","일요일")
+
+a=1
+print(a)
+
+a=123
+print(a)
+
+a=-24
+print(a)
+
+a=0
+print(a)
+
+a=9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+print(a)
+
+a=1.2
+print(a)
+
+a=-3.45
+print(a)
+
+a=0o177
+print(a)
+
+a=0x8ff
+print(a)
+
+a=3
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+a=1
+b=1
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+a=999
+b=1
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+a=99
+b=99
+print(a+b) 
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+print(a%b)
+print(a/b)
+print(a//b)
+
+a=80
+b=75
+c=65
+print((80+75+65)/3)
+d = a + b + c
+print("총점 :",d)
+print("평균점수 :",d/3)
+
+a=89
+b=3
+print("몫 : ", a//b )
+print("나머지 :", a%b)
+
+print(23%2)
+
+print(1003%3)
+
+#1
+print("안녕하새여. 오늘의 날씨는 천둥 입니다.")
+
+#2
+print("지나가던 한 사람이 물엇다.")
+print("\"실례합니다. 여의도역으로 가는 길이 어디입니까?\"")
+print("질문 받은 사람이 대답했다.")
+print("\"I don't know.\"")
+
+#3
+print(' \\   ', "/" , '\\')
+print("  )  (  ')")
+print("(   /   )")
+print('\\   ', "(_)|")
+
+#4
+print("|", '\\' , "_/|")
+print("|q p|", '\\', "}")
+print("( 0 )''''''", '\\')
+print("|''^'''        |")
+print("||_/=", '\\', '\\',"__|")
+
+#5
+a=14
+b=3
+print(a+b) 
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+a=263
+b=38
+print(a+b) 
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+#6
+a=5
+b=8
+c=4
+#d=(a+ )
+
+
+#7
+a=472
+b=5
+c=80
+d=300
+e=a*b
+f=a*c
+g=a*d
+
+print(e+f+g)
