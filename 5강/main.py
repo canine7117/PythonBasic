89e4fb2aa008837490fe34319fad3bf6e35320a3
canine7117 <95954842+canine7117@users.noreply.github.com>
2022-01-26 16:59:51 +0900

    Create main.py

---
+++ 5강/main.py
@@ -0,0 +1,25 @@
+pizza = input('피자 가게가 열렸나요(예/아니요)?')
+chicken = input('치킨 가게가 열렸나요(예/아니요)?')
+편의점 = input('편의점이 열렸나요(예/아니요)?')
+가게 = input('가게가 열렸나요(예/아니요)?')
+if pizza == '예':
+    print( '피자 가게로 가자')
+elif chicken == '예':
+    print('치킨 가게로 가자')
+elif 편의점 == '예':
+    print('편의점으로 가자')
+elif 가게 == '예':
+    print('가게로 가자')
+else : 
+    print('집으로 가자')
+
+
+print (3>5)
+print (3<5)
+print (3==5)
+print (3!=5)
+
+x = 4
+print ( 1<x<5)
+
+print((3==3) and (4!=3))
